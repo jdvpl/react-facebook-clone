989d391459189e4f9d3e2c944298d65f60cb46f1
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-07-20 23:24:54 -0500

    creating the reducer, store, providers

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,16 @@ import App from "./App";
 import "./index.css";
 import "./styles/icons/icons.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
