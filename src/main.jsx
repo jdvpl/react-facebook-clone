ed3a72c494e54f0223d2ac7ecd13543e5e255682
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-07-20 23:03:26 -0500

    routing an different pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./styles/icons/icons.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
